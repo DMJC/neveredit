c1c185924b0c7d91fcff68d3e14a68ce1690e8e5
james <jcarthew@gmail.com>
2020-09-20 11:16:46 +1000

    Initial Import of Code. Updated Tools to use Numpy for Arrays.

---
+++ util/Utils.py
@@ -0,0 +1,38 @@
+'''Some common (non-wx related) util functions'''
+import sys
+
+import PIL.Image,ImageEnhance
+
+use_numeric = True
+
+def iAmOnMac():
+    return sys.platform == 'darwin'
+
+def iAmOnLinux():
+    return 'linux' in sys.platform
+
+#for the below, you also need to change setup.py to exclude the correct package
+#if you're building mac bundles
+
+def getNumPy():
+    if use_numeric:
+        import numpy
+        return numpy
+    else:
+        import numarray
+        return numarray
+
+def getLinAlg():
+    if use_numeric:
+        import LinearAlgebra
+        return LinearAlgebra
+    else:
+        import numarray.linear_algebra
+        return numarray.linear_algebra
+
+def resizeSquareImage(image,size):
+    image = image.resize((size,size),Image.ANTIALIAS)
+    e = ImageEnhance.Sharpness(image)
+    image = e.enhance(13)
+    return image
+
